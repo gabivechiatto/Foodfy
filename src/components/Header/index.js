23190e5af8db9a8a0658c224ad27f99d8e2c7cdd
Gabriela L. T. V. Mesquita <gabriela.mesquita@luizalabs.com>
2022-08-05 16:18:37 -0300

    feat: button animation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import mealsImg from '../../assets/meals.jpg';
 import { CartIcon } from '../../assets/CartIcon';
 import classes from './styles.module.css';
 import CartContext from '../../store/cart-context';
 
 export const Header = (props) => {
+  const [btnHighlight, setBtnHighlight] = useState(false)
   const cartCtx = useContext(CartContext)
+  const { items } = cartCtx
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount
   }, 0)
+
+  const btnClasses = `
+    ${classes['cart-button']}
+    ${btnHighlight ? classes.bump : ''}
+  `
+
+  useEffect(() => {
+    if(cartCtx.items.length === 0){
+      return
+    }
+    setBtnHighlight(true)
+
+    const timer = setTimeout(() => {
+      setBtnHighlight(false)
+    } , 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
 
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>Foodfy</h1>
         <div>
-          <button type="button" className={classes['cart-button']} onClick={props.onShow}>
+          <button type="button" className={btnClasses} onClick={props.onShow}>
             <span className={classes.icon}>
               <CartIcon/>
             </span>
