f35bd363157caebfd91373829f694ef20c9ea883
Gabriela L. T. V. Mesquita <gabriela.mesquita@luizalabs.com>
2022-08-05 15:38:21 -0300

    feat: CartItem component

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,14 +2,32 @@ import React, { useContext } from 'react';
 import { Modal } from '../Modal';
 import classes from './styles.module.css';
 import CartContext from '../../store/cart-context';
+import { CartItem } from './CartItem';
 
 export const Cart = (props) => {
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
   const hasItems = cartCtx.items.length > 0
 
-  const cartItems = <ul className={classes['card-items']}>
-    {cartCtx.items.map((item) => <li>{item.name}</li>)}</ul>
+  const cartItemAddHandler = (item) => {
+
+  }
+
+  const cartItemRemoveHandler = (id) => {
+
+  }
+
+  const cartItems = <ul className={classes['cart-items']}>
+    {cartCtx.items.map((item) =>
+      <CartItem
+      key={item.id}
+      name={item.name}
+      amount={item.amount}
+      price={item.price}
+      onAdd={cartItemAddHandler.bind(null, item)}
+      onRemove={cartItemRemoveHandler.bind(null, item.id)}
+    />)}
+    </ul>
 
   return(
     <Modal onClose={props.onClose}>
